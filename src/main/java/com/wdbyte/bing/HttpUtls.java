40e80aa3bd6ae3838c5ecbe02d057cbc71828570
comdotwww <62149924+comdotwww@users.noreply.github.com>
2022-07-23 17:27:40 +0800

    update config

--- src/main/java/com/wdbyte/bing/HttpUtls.java
+++ src/main/java/com/wdbyte/bing/HttpUtls.java
@@ -26,7 +26,7 @@ public class HttpUtls {
     public static HttpURLConnection getHttpUrlConnection(String url) throws IOException {
         URL httpUrl = new URL(url);
         HttpURLConnection httpConnection = (HttpURLConnection)httpUrl.openConnection();
-        httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
+        httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62");
         return httpConnection;
     }
 
