1c8acfc5040c6038af88ad440f570b28a0f9bbbd
comdotwww <62149924+comdotwww@users.noreply.github.com>
2022-10-18 23:10:23 +0800

    Update FileUtils.java

--- src/main/java/com/wdbyte/bing/FileUtils.java
+++ src/main/java/com/wdbyte/bing/FileUtils.java
@@ -124,7 +124,7 @@ public class FileUtils {
             .collect(Collectors.toList());
         int i = 0;
         for (String date : dateList) {
-            String link = String.format("[%s](https://github.com/comdotwww/bing-wallpaper/tree/main/picture/%s/) | ", date, date);
+            String link = String.format("[%s](/picture/%s/) | ", date, date);
             Files.write(README_PATH, link.getBytes(), StandardOpenOption.APPEND);
             i++;
             if (i % 8 == 0) {
